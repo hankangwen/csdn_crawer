09592a8f634c770e2cb1cb53ca1ff75d31aaeeaa
hankangwen <46420877+hankangwen@users.noreply.github.com>
2022-11-14 09:30:36 +0800

    Update csdn_crawler.py

--- csdn_crawler.py
+++ csdn_crawler.py
@@ -5,10 +5,11 @@ def start():
     headers0 = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36(KHTHL, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
     }
-    # headers2 = {
-    #     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0',
-    # }
+    headers2 = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0',
+    }
 
+    result = []
     url0 = 'https://blog.csdn.net/qq_34035956'
     html_file0 = requests.get(url0, headers=headers0)
     obj_soup0 = bs4.BeautifulSoup(html_file0.text, 'html.parser')
@@ -20,7 +21,10 @@ def start():
             for link1 in obj_soup1.find_all('a'):
                 value1 = "{}".format(link1.get('href'))
                 if value1.find('qq_34035956/article') != -1 & value1.find("comments") == -1:
-                    requests.get(value1, headers=headers0)
+                    result.append(value1)
+                    
+    for link in result:
+        requests.get(value1, headers=headers2)
 
 if __name__ == '__main__':
     start()
